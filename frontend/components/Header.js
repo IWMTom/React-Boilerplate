36064cb4a37593399972a5d48a7468dd42f8cb74
Tom Wilson <tom.wilson@saggezza.com>
2019-10-23 15:54:49 +0100

    Fixes

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,5 +1,7 @@
 import React from "react";
 import NProgress from "nprogress";
+import Router from "next/router";
+import Link from "next/link";
 
 Router.onRouteChangeStart = () => {
 	NProgress.start();
--- frontend/routes.js
+++ frontend/routes.js
@@ -1,4 +1,4 @@
 const routes = require("next-routes");
 
-// module.exports = routes()
-// 	.add("brand", "/brand/:slug");
+module.exports = routes()
+	.add("brand", "/brand/:slug");
